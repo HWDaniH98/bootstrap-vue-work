82514af5e3317b91294fbf6bdf41be0766d2b917
HWDaniH98 <habtom.daniha.daniha30@gmail.com>
2020-09-07 23:31:40 -0700

    Video project roughly duplicated

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,84 @@
 <template>
   <div id="app" class="card">
-    <b-card
+    <!--<b-card
       title = "B-card Page"
     >
       <b-card-text>
-        This a page with a b-card.
+        This a page with a b-card
       </b-card-text>
-    </b-card>
+    </b-card>-->
+
+    <div id = "nav">
+      <top-header></top-header>
+      <!-- <router-link to = "/">Home</router-link>
+      <router-link to = "/about">About</router-link>
+      <router-view/> -->
+    </div>
+    
+    <b-container>
+      <b-row align-v="center">
+        <job-card v-for="job in displayJobs" :key="job.id" :name="job.name"></job-card>
+      </b-row>
+    </b-container>
+
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      first-text="First"
+      prev-text="Prev"
+      next-text="Next"
+      last-text="Last"
+      @input ="paginate(currentPage)"
+    ></b-pagination>
   </div>
 </template>
 
-<script></script>
+<script>
+  import JobCard from "@/components/JobCard.vue";
+  import TopHeader from "@/components/TopHeader.vue";
+  export default
+  {
+    name: "home",
+    components:
+    {
+      "job-card" : JobCard,
+      "top-header" : TopHeader
+    },
+    mounted()
+    {
+      this.fetchData();
+    },
+    data()
+    {
+      return {
+        jobs: [],
+        displayJobs: [],
+        currentPage: 1,
+        rows: 1,
+        perPage: 3
+      }
+    },
+    methods:
+    {
+      async fetchData()
+      {
+        const res = await fetch("jobs.json");
+        const val = await res.json();
+        this.jobs = val;
+        this.displayJobs = val.slice(0, 3);
+        this.rows = this.jobs.length;
+        console.log(val);
+      },
+      paginate(currentPage)
+      {
+        alert(currentPage);
+        const start = (currentPage - 1) * this.perPage;
+        this.displayJobs = this.jobs.slice(start, start + 3);
+      }
+    }
+  };
+</script>
 
 <style>
   #app
